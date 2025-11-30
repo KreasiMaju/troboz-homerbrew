a013b00f15154226d3e3b588d19fcdd0d63e18e9
GitLab CI <ci@gitlab.com>
2025-11-30 20:10:13 +0700

    Update troboz to v0.1.43

--- Formula/troboz.rb
+++ Formula/troboz.rb
@@ -1,15 +1,15 @@
 class Troboz < Formula
   desc "Troboz - Secure tunnel client"
   homepage "https://troboz.com"
-  version "0.1.42"
+  version "0.1.43"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/KreasiMaju/troboz-release/releases/download/v0.1.42/troboz-0.1.42-macos-arm64.tar.gz"
-    sha256 "7dcbf4dfd7d0ce91fdac4cd0db4152ad6c2916ba547205370a2ed6398343deed"
+    url "https://github.com/KreasiMaju/troboz-release/releases/download/v0.1.43/troboz-0.1.43-macos-arm64.tar.gz"
+    sha256 "5fdc0fd8965b9ad0b24f1f14ed99a7fb8593c084a876178bf693ee225c72b7cd"
   else
-    url "https://github.com/KreasiMaju/troboz-release/releases/download/v0.1.42/troboz-0.1.42-macos-x86_64.tar.gz"
-    sha256 "d49f65bf6780da3845b59428c0a4a0736cbaa85736680c900d118fe9140038ec"
+    url "https://github.com/KreasiMaju/troboz-release/releases/download/v0.1.43/troboz-0.1.43-macos-x86_64.tar.gz"
+    sha256 "1027d8be043b68f7fe5039385cab9633ed9432a9f93d472c065c98b76893ef84"
   end
 
   def install
